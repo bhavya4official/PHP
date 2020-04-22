94aed94623a2e1c64818eebad91862ca47036012
Bhavya SIngh <bhavya4official@gmail.com>
2020-04-22 12:41:58 +0530

    Add files via upload

---
+++ login_form_demo/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head><title>login</title>
+<script>
+form {
+  text-align: center;
+}
+</script>
+</head>
+<body>
+<center><h3>Login Form</h3></center><br/>
+<center><form action="welcome.php" method="post">
+Username:<input type=
+"text" name="name"><br/>
+Password:<input type="password" name="pwd"><br/>
+<input type="submit" value="Login">
+<button name="register" type="submit" formaction="register.php" value="register">Register</button>
+</form>
+</center>
+</body>
